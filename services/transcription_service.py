23c33651d162c2ac0f3ef326bef1b0b5145db67a
Morten Ingemann Zeiner <morteningemann86@gmail.com>
2025-02-20 08:51:47 +0100

    add transcription service

---
+++ services/transcription_service.py
@@ -0,0 +1,13 @@
+class TranscriptionService:
+    def __init__(self, groq_client):
+        self.groq_client = groq_client
+
+    def transcribe_audio(self, audio_file):
+        transcription = self.groq_client.audio.transcriptions.create(
+            file=audio_file,
+            model="whisper-large-v3",
+            response_format="json",
+            language="en",
+            temperature=0.0
+        )
+        return transcription.text
