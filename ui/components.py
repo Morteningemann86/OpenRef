11607ef333ed008fcad0ad38887f7493127e05a9
Morten Ingemann Zeiner <morteningemann86@gmail.com>
2025-02-20 08:53:59 +0100

    add component.py

---
+++ ui/components.py
@@ -0,0 +1,79 @@
+"""
+Manages Streamlit components and layouts for the app.
+"""
+
+import streamlit as st
+from ..models.statistics import GenerationStatistics
+
+class NoteSection:
+    def __init__(self, structure, transcript):
+        self.structure = structure
+        self.contents = {title: "" for title in self.flatten_structure(structure)}
+        self.placeholders = {title: st.empty() for title in self.flatten_structure(structure)}
+
+        st.markdown("## Raw transcript:")
+        st.markdown(transcript)
+        st.markdown("---")
+
+    def flatten_structure(self, structure):
+        sections = []
+        for title, content in structure.items():
+            sections.append(title)
+            if isinstance(content, dict):
+                sections.extend(self.flatten_structure(content))
+        return sections
+
+    def update_content(self, title, new_content):
+        try:
+            self.contents[title] += new_content
+            self.display_content(title)
+        except TypeError as e:
+            pass
+
+    def display_content(self, title):
+        if self.contents[title].strip():
+            self.placeholders[title].markdown(f"## {title}\n{self.contents[title]}")
+
+    def return_existing_contents(self, level=1) -> str:
+        existing_content = ""
+        for title, content in self.structure.items():
+            if self.contents[title].strip():  # Only include title if there is content
+                existing_content += f"{'#' * level} {title}\n{self.contents[title]}.\n\n"
+            if isinstance(content, dict):
+                existing_content += self.get_markdown_content(content, level + 1)
+        return existing_content
+
+    def display_structure(self, structure=None, level=1):
+        if structure is None:
+            structure = self.structure
+        
+        for title, content in structure.items():
+            if self.contents[title].strip():  # Only display title if there is content
+                st.markdown(f"{'#' * level} {title}")
+                self.placeholders[title].markdown(self.contents[title])
+            if isinstance(content, dict):
+                self.display_structure(content, level + 1)
+
+    def display_toc(self, structure, columns, level=1, col_index=0):
+        for title, content in structure.items():
+            with columns[col_index % len(columns)]:
+                st.markdown(f"{' ' * (level-1) * 2}- {title}")
+            col_index += 1
+            if isinstance(content, dict):
+                col_index = self.display_toc(content, columns, level + 1, col_index)
+        return col_index
+
+    def get_markdown_content(self, structure=None, level=1):
+        """
+        Returns the markdown styled pure string with the contents.
+        """
+        if structure is None:
+            structure = self.structure
+        
+        markdown_content = ""
+        for title, content in structure.items():
+            if self.contents[title].strip():  # Only include title if there is content
+                markdown_content += f"{'#' * level} {title}\n{self.contents[title]}.\n\n"
+            if isinstance(content, dict):
+                markdown_content += self.get_markdown_content(content, level + 1)
+        return markdown_content
